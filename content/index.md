fa5d6ac89d9120c0ed50e4a5884b76890337ef4f
trung.hoang <trung.hoang@bnksolution.com>
2024-02-11 00:13:06 +0700

    Quartz sync: Feb 11, 2024, 12:13 AM

---
+++ content/index.md
@@ -0,0 +1,14 @@
+---
+title: <% tp.file.title %>
+draft: 
+tags:
+---
+# Coming Soon
+
+We're working hard to bring you an amazing website.
+
+Stay tuned for updates!
+
+---
+
+*Visit us at [hoanghoang.dev](https://hoanghoang.dev)*
--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -6,7 +6,6 @@ import de from "./locales/de-DE"
 import nl from "./locales/nl-NL"
 import ro from "./locales/ro-RO"
 import es from "./locales/es-ES"
-import ar from "./locales/ar-SA"
 import uk from "./locales/uk-UA"
 
 export const TRANSLATIONS = {
@@ -18,26 +17,6 @@ export const TRANSLATIONS = {
   "ro-RO": ro,
   "ro-MD": ro,
   "es-ES": es,
-  "ar-SA": ar,
-  "ar-AE": ar,
-  "ar-QA": ar,
-  "ar-BH": ar,
-  "ar-KW": ar,
-  "ar-OM": ar,
-  "ar-YE": ar,
-  "ar-IR": ar,
-  "ar-SY": ar,
-  "ar-IQ": ar,
-  "ar-JO": ar,
-  "ar-PL": ar,
-  "ar-LB": ar,
-  "ar-EG": ar,
-  "ar-SD": ar,
-  "ar-LY": ar,
-  "ar-MA": ar,
-  "ar-TN": ar,
-  "ar-DZ": ar,
-  "ar-MR": ar,
   "uk-UA": uk,
 } as const
 

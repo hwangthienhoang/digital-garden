ed2d918c83e584b388ebcc76994b0124709a69f3
hwangthienhoang <hwang.thienhoang@gmail.com>
2024-02-14 14:15:22 +0700

    Config code style

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,18 +1,18 @@
-import { defaultTheme } from "@vuepress/theme-default";
-import { defineUserConfig } from "vuepress/cli";
-import { viteBundler } from "@vuepress/bundler-vite";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from 'vuepress/cli'
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
-  lang: "en-US",
+  lang: 'en-US',
 
-  title: "VuePress",
-  description: "My first VuePress Site",
+  title: 'VuePress',
+  description: 'My first VuePress Site',
 
   theme: defaultTheme({
-    logo: "https://vuejs.press/images/hero.png",
+    logo: 'https://vuejs.press/images/hero.png',
 
-    navbar: ["/", "/get-started"],
+    navbar: ['/', '/get-started'],
   }),
 
   bundler: viteBundler(),
-});
+})

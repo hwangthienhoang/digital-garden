2e8415a02af0b8886eda08dc6169ec0b19198a24
hwangthienhoang <hwang.thienhoang@gmail.com>
2024-02-14 15:51:02 +0700

    Init CosmicHazeDriftTheme

---
+++ docs/.vuepress/CosmicHazeDrift.theme.ts
@@ -0,0 +1,25 @@
+import type { Theme } from 'vuepress/core'
+import { getDirname, path } from 'vuepress/utils'
+import { defaultTheme, type DefaultThemeOptions } from '@vuepress/theme-default'
+
+const __dirname = getDirname(import.meta.url)
+
+export const CosmicHazeDriftTheme = (options: DefaultThemeOptions): Theme => {
+  return {
+    name: 'cosmic-haze-drift',
+    extends: defaultTheme(options),
+
+    // override layouts in child theme's client config file
+    // notice that you would build ts to js before publishing to npm,
+    // so this should be the path to the js file
+    // clientConfigFile: path.resolve(__dirname, './client.js'),
+
+    // override component alias
+    alias: {
+      // '@theme/HomeFooter.vue': path.resolve(
+      //   __dirname,
+      //   './components/MyHomeFooter.vue',
+      // ),
+    },
+  }
+}
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,18 +1,20 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defineUserConfig } from 'vuepress/cli'
-import { defaultTheme } from '@vuepress/theme-default'
+import { theme } from './theme.config'
 
 export default defineUserConfig({
   lang: 'en-US',
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: 'HoangHoang.dev',
+  description: 'Digital garden Site',
+  head: [
+    [
+      'link',
+      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.ico' },
+    ],
+  ],
 
-  theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
-
-    navbar: ['/', '/get-started'],
-  }),
+  theme,
 
   bundler: viteBundler(),
 })
